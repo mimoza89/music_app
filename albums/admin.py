3aba3a9d1a7ee1efda5f1308057fa157ea8edd02
mimoza89 <79771345+mimoza89@users.noreply.github.com>
2022-12-15 00:49:29 +0200

    Delete albums directory

--- albums/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from music_app.albums.models import Album
-
-
-# Register your models here.
-@admin.register(Album)
-class AlbumAdmin(admin.ModelAdmin):
-    pass
