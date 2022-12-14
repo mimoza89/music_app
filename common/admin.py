2ab42aa336144466e75c824e4a48071b47b4866e
mimoza89 <79771345+mimoza89@users.noreply.github.com>
2022-12-15 00:49:44 +0200

    Delete common directory

--- common/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from music_app.common.models import Comment
-
-
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    pass
--- common/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from music_app.common.views import index, make_a_comment, all_comments_for_the_singer
-
-urlpatterns = (
-    path('', index, name='index'),
-    path('comment/<int:singer_id>/', make_a_comment, name='make a comment'),
-    path('comments/<int:pk>/', all_comments_for_the_singer, name='comments for the singer')
-)
