1defd2e41ad907c4b35dbd818c98b61d968a6730
mimoza89 <79771345+mimoza89@users.noreply.github.com>
2022-12-15 00:53:46 +0200

    Delete music_app/music_app directory

--- music_app/music_app/singers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from music_app.singers.models import Singer
-
-
-# Register your models here.
-@admin.register(Singer)
-class SingerAdmin(admin.ModelAdmin):
-    pass
